fb9239adcff24f07ec73f35830e8f69971e0c3a6
Chris Kavanagh <ckava3@gmail.com>
2022-10-30 20:28:15 -0400

    css

--- src/pages/Framer.jsx
+++ src/pages/Framer.jsx
@@ -3,8 +3,8 @@ import { motion } from "framer-motion";
 export default function Framer() {
   return (
     <motion.div
-      initial={{ opacity: 0, scale: 0.5, color: "white" }}
-      animate={{ opacity: 1, scale: 1.2, color: "red" }}
+      initial={{ opacity: 0, scale: 0.5 }}
+      animate={{ opacity: 1, scale: 1.2 }}
       transition={{ duration: 2.5 }}
       className="box"
     >
