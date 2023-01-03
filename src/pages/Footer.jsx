8a1d1e147939b286c19edcc16b2315bc12901774
Chris Kavanagh <ckava3@gmail.com>
2023-01-02 21:00:08 -0500

    css

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -7,13 +7,11 @@ import styles from "../styles/Footer.module.css";
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <div className={styles.wrapper}>
-        <h4 className={styles.txt}>&copy;Home Office 2022</h4>
-      </div>
       <div className={styles.socialMedia}>
-        <img src={twitter} width="32px" height="32px" />
-        <img src={facebook} width="32px" height="32px" />
-        <img src={instagram} width="32px" height="32px" />
+        <h3>Social</h3>
+        <img src={twitter} width="24px" height="24px" />
+        <img src={facebook} width="24px" height="24px" />
+        <img src={instagram} width="24px" height="24px" />
       </div>
     </footer>
   );
